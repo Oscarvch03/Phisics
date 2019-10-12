3d5df697186d6dbb97acaefe25f760dfc7a00856
Oscarvch03 <oscar.velasco@urosario.edu.co>
2019-10-12 11:36:59 -0500

    Stellar Drag pA

--- simulation/satellite/A.py
+++ simulation/satellite/A.py
@@ -15,8 +15,10 @@ import forces.forces as fr
 
 GMe = 20
 
-A = 0.3538 # con 0 <= A <= 0.3538, de 11 a 7 orbitas, luego se totea por errores numericos
+A = 0. # con 0 <= A <= 0.3538, de 11 a 7 orbitas, luego se totea por errores numericos
+A2 = 0.1
 print("Con A =", A)
+print("Luego de una orbita, A =", A2)
 
 
 ################################################################################
@@ -57,7 +59,7 @@ m3 = "Midpoint"
 satellite = pt.Particle("Satellite", x0, y0, v0, a0, m) # Revisar por que a0 es 90?
 satellite_force = fr.Forces(universal_gravity, sim_params)
 satellite.set_force(satellite_force)
-euler = sl.Solver(satellite, m2, deltat) # El metodo mas estable es Euler Cromer
+euler = sl.Solver(satellite, m3, deltat) # El metodo mas estable es Euler Cromer
 xpos, ypos, tpos = [], [], []
 cont = 0
 
@@ -76,8 +78,14 @@ for i in range(200000):
     xc1, yc1, vxc1, vyc1, tc1 = satellite.get_state()
     if yc < 0 and yc1 >= 0:
         cont += 1
+        if cont == 1: # Observe que se permite que se realice una orbita con las condiciones iniciales
+                      # y sin A, pero luego se cambia A
+            A = A2
+            sim_params = m, GMe, A
+            satellite_force = fr.Forces(universal_gravity, sim_params)
+            satellite.set_force(satellite_force)
 
-print("Method:", m2, ", dt:", deltat)
+print("Method:", m3, ", dt:", deltat)
 # for j in em:
 #     print("Energia Mec Tot:", j)
 print("Energia Mec Tot In:", em[0], ", Energia Mec Tot Fin:", em[-1])
@@ -90,28 +98,55 @@ print("Orbitas:", cont)
 # print("Punto donde emt es max:, x = ", round(emmx, 5), "& y =", round(emmy, 5))
 # print("Con t =", round(emmt, 4), ", emtm =", max(em))
 
+# GRAFICA xVSy
+fig, ax = plt.subplots()
+ax.plot(xpos, ypos, '--', label=m3)
 
-# fig, ax = plt.subplots()
-# ax.plot(xpos, ypos, '--', label=m2)
-#
-# ax.set(xlabel='x (a.u.)', ylabel='y (a.u.)',
-#        title='Projectile motion with drag, deltat: ' + str(deltat) + ', Orbits:' + str(cont))
-# ax.grid()
+ax.set(xlabel='x (a.u.)', ylabel='y (a.u.)',
+       title='Simulation Planet Motion with Stellar Drag, deltat: ' + str(deltat) + ', Orbits:' + str(cont))
+ax.grid()
 
+# GRAFICA tVSemt
 # fig, ax = plt.subplots()
 # ax.plot(tpos, em, '--', label=m2)
 #
 # ax.set(xlabel='t (yr)', ylabel='emt',
-#        title='Simulation Planet Motion, deltat: ' + str(deltat) + ', Orbits:' + str(cont))
+#        title='Simulation Planet Motion with Stellar Drag, deltat: ' + str(deltat) + ', Orbits:' + str(cont))
 # ax.grid()
 
-fig, ax = plt.subplots()
-ax.plot(tpos, vel, '--', label=m2)
-
-ax.set(xlabel='t (yr)', ylabel='v(E.U / h)',
-       title='Simulation Planet Motion, deltat: ' + str(deltat) + ', Orbits:' + str(cont))
-ax.grid()
+# GRAFICA tVSvel
+# fig, ax = plt.subplots()
+# ax.plot(tpos, vel, '--', label=m2)
+#
+# ax.set(xlabel='t (yr)', ylabel='v(E.U / h)',
+#        title='Simulation Planet Motion with Stellar Drag, deltat: ' + str(deltat) + ', Orbits:' + str(cont))
+# ax.grid()
 
 plt.legend()
-# plt.savefig("p1Midpoint.jpg")
+plt.savefig("AresultMidpoint.PNG")
 plt.show()
+
+# Observe que con x0 = 1, y0 = 1, vx = 0, vy = sqrt(20), deltat = 0.01/128, A = 0
+# tenemos una orbita circular, grafica orbitaCircSinA
+
+# Ahora con A = 0.3538, el satelite hace 6 orbitas, luego se estrella con la
+# tierra, grafica orbitaCircConA
+
+# El metodo mas estable es Euler Cromer, por ende fue elegido para realizar las
+# modificaciones con Arrastre Estelar
+
+# Sabemos que la velocidad es tangencial a la orbita del satelite, y que la
+# fuerza de arrastre es opuesta a la velocidad, asi esta fuerza tiende a frenar
+# el satelite, scandolo de su orbita generando un choque en un tiempo determinado
+# con el planeta sobre el cual esta orbitando, en este caso La Tierra.
+
+# Graficas tVSemt, tVSvel
+
+# Para ver como cambian los resultados dependiendo el integrador numerico usado,
+# ver graficas: resulEuler, resulMidpoint.
+
+# resultEuler: Para el metodo euler, la simulacion se totea completamente con
+# A = 0.3536, asi que hay que bajar A, ahora A = 0.16
+
+# resultMidpoint: Para el metodo Midpoint, simulacion se totea completamente con
+# A = 0.3536, asi que hay que bajar A, ahora A = 0.1
