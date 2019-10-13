4573d581df71cb78be9a60779fa01cbf31f61210
Oscarvch03 <oscar.velasco@urosario.edu.co>
2019-10-12 22:24:18 -0500

    Satellite pC

--- simulation/satellite/C.py
+++ simulation/satellite/C.py
@@ -1,4 +1,4 @@
-# B.py
+# C.py
 
 ################################################################################
 # LIBRERIAS IMPORTADAS Y VARIABLES GLOBALES
@@ -15,9 +15,9 @@ import forces.forces as fr
 
 GMe = 20
 
-W = 0. # 3% de la ace, asi a = v**2 / r, a = (vx**2 + vy**2) / np.sqrt(x**2 + y**2)
-       # a = 20, 3% de 20 es 0.6
-W2 = 0.6
+W = 0.
+W2 = 0.6 # 3% de la ace, asi a = v**2 / r, a = (vx**2 + vy**2) / np.sqrt(x**2 + y**2)
+         # a = 20, 3% de 20 es 0.6
 print("Con W =", W)
 print("Luego de una orbita, W =", W2)
 
@@ -35,8 +35,8 @@ def universal_gravity(state, params):
     mass, gu, W = params
     r2 = x**2 + y**2
     r3 = np.sqrt(r2) * r2
-    ax = (-gu * x / r3) + W
-    ay = (-gu * y / r3)
+    ax = (-gu * x / r3) + W * np.cos(np.radians(45))
+    ay = (-gu * y / r3) + W * np.sin(np.radians(45))
     return vx, vy, ax, ay, 1.
 
 def total_mechanic_energy(v, m, x, y): # cambiar con A & W, con A no cambia,
@@ -48,9 +48,10 @@ def total_mechanic_energy(v, m, x, y): # cambiar con A & W, con A no cambia,
 ################################################################################
 # BLOQUE PRINCIPAL DE INSTRUCCIONES
 
-v0, vy = cond_init(1, 0, 0)
+# v0, vy = cond_init(1, 0, 0) #orbita circular
+k = 1.5 # orbita eliptica
 # print(v0)
-x0, y0, v0, a0, m = 1., .0, v0, 90., 1.
+x0, y0, v0, a0, m = k, .0, k, 90., 1.
 sim_params = m, GMe, W
 deltat = 0.01/128
 
@@ -95,12 +96,12 @@ print("Energia Mec Tot In:", em[0], ", Energia Mec Tot Fin:", em[-1])
 print("Orbitas:", cont)
 
 
-# emmt = tpos[em.index(min(em))]
-# # print(emmt)
-# emmx = xpos[tpos.index(emmt)]
-# emmy = ypos[tpos.index(emmt)]
-# print("Punto donde emt es min:, x = ", round(emmx, 5), "& y =", round(emmy, 5))
-# print("Con t =", round(emmt, 4), ", emtm =", min(em))
+emmt = tpos[em.index(max(em))]
+# print(emmt)
+emmx = xpos[tpos.index(emmt)]
+emmy = ypos[tpos.index(emmt)]
+print("Punto donde emt es min:, x = ", round(emmx, 5), "& y =", round(emmy, 5))
+print("Con t =", round(emmt, 4), ", emtm =", max(em))
 
 
 # GRAFICA xVSy
@@ -128,21 +129,12 @@ ax.grid()
 # ax.grid()
 
 plt.legend()
-plt.savefig("BemtConW.PNG")
+plt.savefig("CemtConW.PNG")
 plt.show()
 
-# Igual que con el arrastre estelar, si W != 0, el satelite tiende a salir
-# de su orbita original, ya que esta nueva fuerza va en la direccion x, asi
-# en un tiempo determinado se produce un choque con la tierra, y aqui la
-# simulacion se totea
 
-# Observe que con x0 = 1, y0 = 0, vx = 0, vy = sqrt(20), deltat = 0.01/128, W = 0
-# tenemos una orbita circular, grafica orbitaCircSinW
+# Observe que con x0 = 1.5, y0 = 1, vx = 0, vy = 1.5, deltat = 0.01/128, W = 0
+# tenemos una orbita eliptica, grafica orbitaElipSinW
 
-# Ahora con W = 0.6, el satelite hace 7 orbitas, luego se estrella con la
-# tierra, grafica orbitaCircConW, ademas de la misma grafica con zoom
-
-# Como la orbita sin W es idealmente circular, la energia mecanica se conserva
-# casi de forma exacta, grafica emtSinW
-
-# Con W = 0.6, la energia mecanica cambia en ciertos puntos, grafica emtConW
+# Ahora con W = 0.6, el satelite hace 13 orbitas, luego se estrella con la
+# tierra, grafica orbitaElipSinW, ademas de la misma grafica con zoom
